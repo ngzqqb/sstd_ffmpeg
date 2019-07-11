df1dcc988f19f6aa37ddbe2ae150d4013214e5e2
ngzHappy <zhaixueqiang@hotmail.com>
2019-07-11 10:29:21 +0800

    +update

--- install_ffmpeg.pro
+++ install_ffmpeg.pro
@@ -1,11 +1,18 @@
 
+TEMPLATE = app
+CONFIG += c++17
+CONFIG += console
 
+QT += core
+QT += gui
 
+### SSTD_LIBRARY_OUTPUT_PATH
+include($$PWD/../sstd_library/sstd_build_path.pri)
 
+DESTDIR = $${SSTD_LIBRARY_OUTPUT_PATH}
+DEFINES *= THE_SOURCE_DIR=\\\"$${PWD}\\\"
 
-
-
-
+SOURCES += $$PWD/main.cpp
 
 
 
---
+++ main.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <string_view>
+#include <iostream>
+#include <fstream>
+#include <QtCore/QtCore>
+#include <array>
+
+int main(int argc,char ** argv){
+
+    using namespace std::string_view_literals;
+#if defined (Q_OS_WIN)
+    QCoreApplication varApp{argc,argv};
+
+    const QDir varTarget{ varApp.applicationDirPath() };
+
+    const QDir varTargetSource{ THE_SOURCE_DIR };
+
+    QDirIterator varIt{
+        varTargetSource.absoluteFilePath(QStringLiteral("ffmpeg_windows/bin")),
+                QDir::Files
+    };
+
+    while( varIt.hasNext() ){
+        varIt.next() ;
+        const auto varFileInfo = varIt.fileInfo();
+        if(varFileInfo.suffix() == QStringLiteral("dll") ){
+            const auto varFrom = varFileInfo.absoluteFilePath();
+            const auto varTo = varTarget.absoluteFilePath(varFileInfo.fileName());
+            QFile::copy( varFrom , varTo );
+#if defined (_DEBUG)
+            qDebug() << varFrom << varTo ;
+#endif
+        }
+    }
+
+#else
+    std::cout << "under linux you should install ffmpeg yourself"sv << std::endl;
+#endif
+
+    return 0;
+
+}
+
